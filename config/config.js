399b5c843884868917d8beb6210b748e4a28048b
guilhermemarciano <guilherme_marciano@outlook.com.br>
2024-02-07 08:31:52 -0300

    feat: configure principal table to show report summary and add id selection listenter

--- config/config.js
+++ config/config.js
@@ -2,12 +2,12 @@ const reportMock = [
     {
         id: 1,
         name: "RP0001",
-        itemsCount: 6        
+        sampleCount: 6        
     },
     {
         id: 2,
         name: "RP0002",
-        itemsCount: 12        
+        sampleCount: 12        
     }
 ]
 
@@ -20,13 +20,36 @@ reportMock.forEach(
         
         let id = document.createElement("td");
         id.textContent = r.id;
-
         newDataElement.appendChild(id);
+
+        let name = document.createElement("td");
+        name.textContent = r.name;
+        newDataElement.appendChild(name);
+
+        let sampleCount = document.createElement("td");
+        sampleCount.textContent = r.sampleCount;
+        newDataElement.appendChild(sampleCount);
+
+        let actions = document.createElement("td");
+        newDataElement.appendChild(actions);
+
+
+        let btnSelectReport = document.createElement("button");
+        btnSelectReport.className = "btn-default";
+        btnSelectReport.textContent = "Selecionar";
+        btnSelectReport.addEventListener("click", () => selectReport(r.id));
+
+        actions.appendChild(btnSelectReport);
+        
         divReportSummary.appendChild(newDataElement);
 
-        console.log(r.name);
     }
 )
+
+function selectReport(id) {
+    console.log("Visualizar amostras para o relatório com o ID:", id);
+    // Implemente a lógica para visualizar amostras aqui
+}
 
 
 
